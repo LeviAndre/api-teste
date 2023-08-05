eed963cebc4b306170310db400ed46ab2713baf5
Andre Levi <andre.levi@novafutura.com.br>
2023-08-05 16:09:59 -0300

    main controller/class

---
+++ FilmesAPI/FilmesAPI/Controllers/MovieController.cs
@@ -0,0 +1,17 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : ControllerBase
+    {
+        private static List<Movie> movies = new List<Movie>();
+
+        [HttpPost]
+        public void postMovie([FromBody] Movie movie) {
+            movies.Add(movie);
+        }
+    }
+}
---
+++ FilmesAPI/FilmesAPI/Models/Movie.cs
@@ -0,0 +1,11 @@
+﻿namespace FilmesAPI.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Gender { get; set; }
+        public int Duration { get; set; }
+        public int Score { get; set; }
+    }
+}
